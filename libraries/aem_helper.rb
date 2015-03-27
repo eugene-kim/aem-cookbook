9a7d1d32a9e4010388b8dec3484c9fd8ba68a461
Eugene Kim <eugene.kim@eugenekim-wsl2.internal.salesforce.com>
2015-03-27 14:52:15 -0700

    add libraries dir and get_jar_opts helper function

---
+++ libraries/aem_helper.rb
@@ -0,0 +1,9 @@
+def get_jar_opts(jar_opts)
+	if jar_opts.any?
+    jar_opts_string = '-r'
+		jar_opts.each do |jar_opt|
+      jar_opts_string << ' ' + jar_opt
+		end
+    jar_opts_string
+	end
+end
