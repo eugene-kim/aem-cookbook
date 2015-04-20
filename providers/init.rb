3ac86b224bc95f9ba2bd50731079058fb9453f3c
Eugene Kim <eugene.kim@eugenekim-wsl2.internal.salesforce.com>
2015-04-20 16:14:01 -0700

    jar_opts are successfully passed through to init.erb now

--- providers/init.rb
+++ providers/init.rb
@@ -22,16 +22,17 @@ action :add do
   vars = {}
   service_name = new_resource.service_name
   var_list = [
-    :aem_options, :default_context, :runnable_jar, :base_dir, :jvm_opts, :jar_opts
+    :aem_options, :default_context, :runnable_jar, :base_dir, :jvm_opts, :jar_opts, :jar_opts_runmodes
   ]
 
   #take value passed to provider, or node attribute
   var_list.each do |var|
     vars[var] = new_resource.send(var) || node[:aem][var]
   end
-  # Chef::Log.info get_jar_opts(new_resource.jar_opts, new_resource.jar_opts_runmodes)
-  Chef::Log.info "the result of get_jar_opts(): #{get_jar_opts}"
-  Chef::Log.info "jar opts attribute after get_jar_opts is run: #{node['aem']['author']['jar_opts']}"
+  Chef::Log.info "vars before get_jar_opts was called: #{vars}"
+  vars[:jar_opts] = [get_jar_opts(vars[:jar_opts].dup, vars[:jar_opts_runmodes].dup)]
+
+  Chef::Log.info "the updated vars list: #{vars}"
 
   template "/etc/init.d/#{service_name}" do
     cookbook 'aem'
@@ -42,30 +43,9 @@ action :add do
   end
 end
 
-# # appends runmodes to an existing run_modes jar_opt or creates one if it doesn't exist
-# def get_jar_opts(jar_opts, jar_opts_runmodes)
-#   run_modes = jar_opts.select{|jar_opt| jar_opt.include? '-r'}  
-#   if run_modes.any?
-#     run_modes_string = run_modes[0]
-#     run_modes_index = jar_opts.index run_modes_string
-#     jar_opts_runmodes.each do |jar_opts_runmode|
-#       run_modes_string << " #{jar_opts_runmode}"
-#     end
-#     jar_opts[run_modes_index] = run_modes_string
-#   else # a jar_opt with 
-#     jar_opts_string = '-r'
-#     jar_opts_runmodes.each do |runmode|
-#       jar_opts_string << " #{runmode}"
-#     end
-#     jar_opts << jar_opts_string
-#   end
-# end
-
-def get_jar_opts
-  jar_opts = node['aem']['author']['jar_opts']
-  jar_opts_runmodes = node['aem']['author']['jar_opts_runmodes']
+# appends runmodes to an existing run_modes jar_opt or creates one if it doesn't exist
+def get_jar_opts(jar_opts, jar_opts_runmodes)
   run_modes = jar_opts.select{|jar_opt| jar_opt.include? '-r'}  
-  Chef::Log.info "run_modes: #{run_modes}"
   if run_modes.any?
     run_modes_string = run_modes[0]
     run_modes_index = jar_opts.index run_modes_string
