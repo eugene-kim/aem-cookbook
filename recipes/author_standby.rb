241797e4e12dc6720aef205163dda1e8b86f1e1b
Eugene Kim <eugene.kim@eugenekim-ltm.internal.salesforce.com>
2015-05-14 01:05:40 -0700

    remove false comment

--- recipes/author_standby.rb
+++ recipes/author_standby.rb
@@ -16,8 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# the aem-author service should be stopped at this point.
-
 include_recipe "aem::_base_aem_setup"
 
 base_dir = node[:aem][:author][:base_dir]
